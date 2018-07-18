cea48420c447460424977ab8f950da20f47cd96c
balasailakshmi <41284575+balasailakshmi@users.noreply.github.com>
2018-07-18 15:54:55 +0530

    NumberSquarePattern

---
+++ pat.java
@@ -0,0 +1,62 @@
+
+import java.util.*;
+import java.io.*;
+class pat
+{
+ public static void main(String args[])
+  {
+    int N, i, j;
+    Scanner s=new Scanner(System.in);
+    
+
+    System.out.println("Enter N: ");
+    N=s.nextInt();
+   for(i=N; i>=1; i--)
+   {
+       
+        for(j=N; j>i; j--)
+        {
+            System.out.print(j);
+        }
+
+       
+        for(j=1; j<=(i*2-1); j++)
+        {
+          System.out.print(i);
+        }
+
+        
+        for(j=i+1; j<=N; j++)
+        {
+            System.out.print(j);
+        }
+
+        System.out.println();
+    }
+
+for(i=1;i<N;i++)
+    {
+    
+        for(j=N; j>i; j--)
+        {
+            System.out.print(j);
+        }
+
+        
+        for(j=1; j<=(i*2-1); j++)
+        {
+            System.out.print( i+1);
+        }
+
+   
+        for(j=i+1; j<=N; j++)
+        {
+            System.out.print(j);
+        }
+
+        System.out.println();
+    }
+
+ 
+}
+}
